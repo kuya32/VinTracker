53134f7c562b4d1790294f9f6f1624200ab8ddd9
Marchael Acode <m.acode@outlook.com>
2022-06-08 23:58:36 -0700

    Working on the navigation drawer.

---
+++ app/src/main/java/com/github/kuya32/vintracker/NavigationDrawer.kt
@@ -0,0 +1,52 @@
+package com.github.kuya32.vintracker
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.time.format.TextStyle
+
+@Composable
+fun DrawerHeader() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Header", fontSize = 60.sp)
+    }
+}
+
+@Composable
+fun DrawerBody(
+items: List<MenuItem>,
+modifier: Modifier = Modifier,
+itemTextStyle: TextStyle = TextStyle(fontSize = 18.sp),
+onItemClick: (MenuItem) -> Unit
+) {
+    LazyColumn(modifier) {
+        items(items) { item ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        onItemClick(item)
+                    }
+                    .padding(16.dp)
+            ) {
+                Icon(imageVector = item.icon, contentDescription = item.contentDescription)
+            }
+        }
+    }
+}
