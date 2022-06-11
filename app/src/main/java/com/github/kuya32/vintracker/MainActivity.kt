cb1c614105e61cbd9ddfa66b74ad998cc3c8b8b1
Marchael Acode <m.acode@outlook.com>
2022-06-10 23:48:02 -0700

    Refactoring navigation drawer. Adding more menu items in body and refactoring header.

--- app/src/main/java/com/github/kuya32/vintracker/MainActivity.kt
+++ app/src/main/java/com/github/kuya32/vintracker/MainActivity.kt
@@ -3,16 +3,20 @@ package com.github.kuya32.vintracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Help
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.PersonAdd
+import androidx.compose.material.icons.filled.PersonSearch
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.github.kuya32.vintracker.ui.theme.VinTrackerTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -20,42 +24,59 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             VinTrackerTheme {
-                Scaffold(
-                    content = { innerPadding ->
-                        ModalNavigationDrawer(
-                            modifier = Modifier
-                                .padding(innerPadding),
-                            drawerContent = {
-                                DrawerHeader()
-                                DrawerBody(
-                                    items = listOf(
-                                        MenuItem(
-                                            id = "home",
-                                            title = stringResource(id = R.string.home),
-                                            contentDescription = stringResource(id = R.string.go_to_home_screen),
-                                            icon = Icons.Default.Home
-                                        ),
-                                        MenuItem(
-                                            id = "settings",
-                                            title = stringResource(id = R.string.settings),
-                                            contentDescription = stringResource(id = R.string.go_to_settings_screen),
-                                            icon = Icons.Default.Home
-                                        ),
-                                        MenuItem(
-                                            id = "help",
-                                            title = stringResource(id = R.string.help),
-                                            contentDescription = stringResource(id = R.string.get_help),
-                                            icon = Icons.Default.Home
-                                        )
-                                    ),
-                                    onItemClick = {
-                                        println("Clicked on ${it.title}")
+                val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+                val scope = rememberCoroutineScope()
+                ModalNavigationDrawer(
+                    drawerState = drawerState,
+                    drawerContent = {
+                        DrawerHeader()
+                        DrawerBody(
+                            items = listOf(
+                                MenuItem(
+                                    id = "home",
+                                    title = stringResource(id = R.string.home),
+                                    contentDescription = stringResource(id = R.string.go_to_home_screen),
+                                    icon = Icons.Default.Home
+                                ),
+                                MenuItem(
+                                    id = "add a client",
+                                    title = stringResource(id = R.string.add_a_client),
+                                    contentDescription = stringResource(id = R.string.add_a_client),
+                                    icon = Icons.Default.PersonAdd
+                                ),
+                                MenuItem(
+                                    id = "search for client",
+                                    title = stringResource(id = R.string.search_for_client),
+                                    contentDescription = stringResource(id = R.string.search_for_client),
+                                    icon = Icons.Default.PersonSearch
+                                ),
+                                MenuItem(
+                                    id = "help",
+                                    title = stringResource(id = R.string.help),
+                                    contentDescription = stringResource(id = R.string.get_help),
+                                    icon = Icons.Default.Help
+                                )
+                            ),
+                            onItemClick = {
+                                println("Clicked on ${it.title}")
+                            }
+                        )
+                    },
+                    content = {
+                        Scaffold(
+                            topBar = {
+                                AppBar(
+                                    onNavigationIconClick = {
+                                        scope.launch {
+                                            drawerState.open()
+                                        }
                                     }
                                 )
+                            },
+                            content = { innerPadding ->
+                                Box(modifier = Modifier.padding(innerPadding))
                             }
-                        ) {
-
-                        }
+                        )
                     }
                 )
             }
--- app/src/main/java/com/github/kuya32/vintracker/NavigationDrawer.kt
+++ app/src/main/java/com/github/kuya32/vintracker/NavigationDrawer.kt
@@ -1,14 +1,27 @@
 package com.github.kuya32.vintracker
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -16,13 +29,32 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun DrawerHeader() {
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp),
-        contentAlignment = Alignment.Center
+            .padding(16.dp)
+            .drawBehind {
+                val strokeWidth = 2f
+                val x = size.width - strokeWidth
+                val y = size.height - strokeWidth
+
+                drawLine(
+                    color = Color(0xff003314),
+                    start = Offset(0f, y),
+                    end = Offset(x, y),
+                    strokeWidth = strokeWidth
+                )
+            }
     ) {
-        Text(text = "Header", fontSize = 60.sp)
+        Image(
+            imageVector = Icons.Default.Person,
+            contentDescription = stringResource(id = R.string.profile_image),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(32.dp)
+                .clip(CircleShape)
+                .border(1.dp, Color(0xff003314), CircleShape)
+        )
     }
 }
 
@@ -45,7 +77,9 @@ fun DrawerBody(
             ) {
                 Icon(
                     imageVector = item.icon,
-                    contentDescription = item.contentDescription
+                    contentDescription = item.contentDescription,
+                    modifier = Modifier
+                        .padding(end = 32.dp)
                 )
                 Spacer(modifier = Modifier.height(16.dp))
                 Text(
